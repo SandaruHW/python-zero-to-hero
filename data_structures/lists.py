6b27d99bd963550df9b9a5eb84496d29f9b3fceb
Sandaru Sathsara <sandarusathsarahw@gmail.com>
2026-02-13 21:59:19 +0530

    Add lists and user input demo scripts

---
+++ data_structures/lists.py
@@ -0,0 +1,28 @@
+#lists
+
+x = [12, 434, 565, 77]
+
+print(x)
+print(x[0]) # 12
+print(x[1]) # 434
+#print(x[4]) # IndexError: list index out of range
+
+x.append(100)
+print(x) # [12, 434, 565, 77, 100]
+
+x.insert(2, 200)
+print(x) # [12, 434, 200, 565, 77, 100]
+
+x.remove(434)
+print(x) # [12, 200, 565, 77, 100]
+
+x.pop(3)
+print(x) # [12, 200, 565, 100]
+
+y = [1, 2, 3]
+z = x + y
+print(z) # [12, 200, 565, 100, 1, 2, 3]
+
+is_12_in_z = 12 in z
+print(is_12_in_z) # True
+
---
+++ getting_user_inputs/demo.py
@@ -0,0 +1,7 @@
+print("Enter your name:", end = "")
+name = input()
+
+print("Enter your age:", end="")
+age = input()
+
+print("Hello", name  + "!" +  " You are", age, "years old!")
